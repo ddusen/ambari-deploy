78775ae560dd524554aaa91a80847f23ce2a45d6
杜森 <sen.du@petgleam.com>
2023-06-15 16:43:10 +0800

    [ADD] 项目打包:build.sh

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#author: Sen Du
+#email: dusen@gennlife.com
+#created: 2023-06-16 21:00:00
+#updated: 2023-06-16 21:00:00
+
+set -e 
+source 00_env
+
+# 自动压缩文件
+function zipfile() {
+    cd ../
+    tar -zcvf ambari.tar.gz ambari
+    cd ambari/
+}
+
+function main() {
+    echo -e "$CSTART build.sh$CEND"
+
+
+    echo -e "$CSTART> zipfile$CEND"
+    zipfile
+}
+
+main
