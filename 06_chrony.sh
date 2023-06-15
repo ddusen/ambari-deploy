0343108da502883c0a7c50fcff658831b0848209
杜森 <sen.du@petgleam.com>
2023-06-15 17:10:54 +0800

    [ADD] 使用 chrony 配置时钟服务，因为 ntp 不支持 rocky8.7

---
+++ 06_chrony.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+#author: Sen Du
+#email: dusen@gennlife.com
+#created: 2023-04-16 14:00:00
+#updated: 2023-04-16 14:00:00
+
+set -e 
+source 00_env
+
+# 备份 chrony config
+function backup_chrony_config() {
+    cat config/vm_info | grep -v "^#" | grep -v "^$" | while read ipaddr name passwd
+    do
+        echo -e "$CSTART>>>>$ipaddr$CEND"
+        ssh -n $ipaddr "cp /etc/chrony.conf /etc/chrony.conf.bak" || true
+    done
+}
+
+# 安装 chrony
+function install_chrony() {
+    cat config/vm_info | grep -v "^#" | grep -v "^$" | while read ipaddr name passwd
+    do
+        echo -e "$CSTART>>>>$ipaddr$CEND"
+        ssh -n $ipaddr "yum install -y chrony" || true
+    done
+}
+
+# 配置 chrony clients
+function config_chrony_clients() {
+    cat config/vm_info | grep -v "^#" | grep -v "^$" | while read ipaddr name passwd
+    do
+        echo -e "$CSTART>>>>$ipaddr$CEND"
+        scp config/chrony_client $ipaddr:/etc/chrony.conf
+        ssh -n $ipaddr "sed -i 's/TODO_SERVER_IP/$LocalIp/g' /etc/chrony.conf"
+    done
+}
+
+# 配置 chrony server
+function config_chrony_server() {
+    echo -e "$CSTART>>>>$(hostname -I)$CEND"
+    cp config/chrony_server /etc/chrony.conf
+    sed -i "s/TODO_SERVER_IP/$(echo $LocalIp | awk -F. '{OFS="."; $NF=0; print}')/g" /etc/chrony.conf
+}
+
+# 重启 chrony 服务
+function restart_chrony() {
+    cat config/vm_info | grep -v "^#" | grep -v "^$" | while read ipaddr name passwd
+    do
+        echo -e "$CSTART>>>>$ipaddr$CEND"
+        ssh -n $ipaddr "systemctl start chronyd" || true
+        ssh -n $ipaddr "systemctl enable --now chronyd.service" || true
+    done
+}
+
+function main() {
+    echo -e "$CSTART>06_chrony.sh$CEND"
+
+    echo -e "$CSTART>>backup_chrony_config$CEND"
+    backup_chrony_config
+    
+    echo -e "$CSTART>>remove_old_chrony$CEND"
+    remove_old_chrony
+
+    echo -e "$CSTART>>install_chrony$CEND"
+    install_chrony
+
+    echo -e "$CSTART>>config_chrony_clients$CEND"
+    config_chrony_clients
+
+    echo -e "$CSTART>>config_chrony_server$CEND"
+    config_chrony_server
+
+    echo -e "$CSTART>>restart_chrony$CEND"
+    restart_chrony
+
+    #echo -e "$CSTART>>force_refresh_chrony$CEND"
+    #force_refresh_chrony
+}
+
+main
